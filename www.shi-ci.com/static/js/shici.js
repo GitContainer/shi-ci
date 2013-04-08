cc478b2738df9abcd19c4194acf53aec0e582665
Michael Liao <askxuefeng@gmail.com>
2013-04-08 12:05:08 +0800

    update static resource

--- www.shi-ci.com/static/js/shici.js
+++ www.shi-ci.com/static/js/shici.js
@@ -1,6 +1,18 @@
+function make_poem_boxes(ps) {
+    var L = []
+    $.each(ps, function(i, p) {
+        L.push('<div class="item"><div class="item-header"></div><div class="item-body"><p>');
+        L.push(p.name);
+        L.push('</p><p>');
+        L.push(p.content);
+        L.push('</p></div><div class="item-footer"></div></div>');
+    });
+    return L.join('');
+}
+
 $(function() {
     $(window).scroll(function() {
-        if ($(window).scrollTop() > 500) {
+        if ($(window).scrollTop() > screen.height * 2) {
             $('div.go-top').show();
         }
         else {
@@ -10,9 +22,19 @@ $(function() {
     $('div.go-top').click(function() {
         $('html, body').animate({scrollTop: 0}, 1000);
     });
+    $('div.condition span').click(function() {
+        $(this).parent().children('.selected').removeClass('selected');
+        $(this).addClass('selected');
+    });
     $('a.bigbang').click(function() {
         $('body>div').fadeOut(1000);
         var img = $('body').css('background-image');
         $('body').css('background-color', '#eddec2').css('background-image', 'none').append('<div style="width:694px;text-align:center;margin:20px auto;"><a href="javascript:location.reload()">╮（╯◇╰）╭ 中华诗词 ╮（╯◇╰）╭</a></div><div style="background-image:' + img + ';width:694px;height:1000px;border:1px solid #a38a54;margin:20px auto 20px auto;"></div>');
+    });
+    $('#poems').masonry({
+        itemSelector : '.item',
+        columnWidth : 166,
+        gutterWidth: 30,
+        isResizable: false,
     });
 });
